8047eff03c7b016d1d329ad1e179b4127ddb61fd
sulaymon1404 <sulaymonmakhsumow@gmail.com>
2025-02-07 15:56:24 +0500

    init commit

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,9 @@
+import { BuildOptions } from "./types/config";
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+
+export function buildDevServer ({isDev,port}:BuildOptions):DevServerConfiguration{
+    return {
+        port,
+        open:true
+    }
+}
